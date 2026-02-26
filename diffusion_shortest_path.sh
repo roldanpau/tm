5aae3c89b9e99fd3394a3f0392dd9f2e5e2cc197
Pablo Roldan <pablo.roldan@upc.edu>
2026-02-26 16:57:15 +0100

    Improved documentation

--- diffusion_shortest_path.sh
+++ diffusion_shortest_path.sh
@@ -3,8 +3,8 @@ IMGDIR=$PAPERDIR/images
 
 make phase_port_SM diffusion_shortest_path
 
-#./phase_port_SM 1 0 7.5 100 1000 1 > phase_port_SM.res
-#./diffusion_shortest_path 1 3.14 5 0 > diffusion_shortest_path.res
+# The initial point (I=1, \phi=5.026548) and final point (I=7, \phi=0) below 
+# where found using program diffusion_shortest_path_Iini_Ifin
 ./diffusion_shortest_path 1 5.026548 7 0 > diffusion_shortest_path.res
 gnuplot diffusion_shortest_path.plt
 mv diffusion_shortest_path.png $IMGDIR
